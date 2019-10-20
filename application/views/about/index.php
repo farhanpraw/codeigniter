dbea14ce9a08e6d75294f94540c77854e3968cf8
Riangembira <riangembira23@gmail.com>
2019-10-21 00:34:59 +0700

    initial commit codeigniter

---
+++ application/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+
+		<h1 class="mt-5">About Me</h1>
+		<img src="<?= base_url(); ?>assets/img/kobe2.JPG" alt="FarhanPraw" width="300" class="rounded-circle shadow" >
+		<p>Halo, Nama Saya <?= $nama ; ?>, umur saya <?= $umur ; ?> tahun, saya selalu <?= $pekerjaan ; ?> </p>
+</div>
